f4764d1284bb57920140bb79cd9a0cd69ea768a3
ddcsoftdev <ddcsoftwaredev@gmail.com>
2025-01-24 12:33:15 +0100

    adding logout button

--- frontend/chat-app-frontend/src/app/dashboard/user-settings/user-settings.component.ts
+++ frontend/chat-app-frontend/src/app/dashboard/user-settings/user-settings.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
   imports: [],
   templateUrl: './user-settings.component.html',
-  styleUrl: './user-settings.component.scss'
+  styleUrl: './user-settings.component.scss',
 })
 export class UserSettingsComponent {
+  constructor(private authService: AuthService) {}
 
+  handleLogout(): void {
+    this.authService.logout();
+    window.location.reload();
+  }
 }
